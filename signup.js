162a41794efeb52a0484d722950902f7249996ce
nagiarjun05 <nagiarjun05@gmail.com>
2022-12-12 13:00:37 +0530

    post user request

--- signup.js
+++ signup.js
@@ -1,13 +1,28 @@
 const userName=document.getElementById("user-name");
 const userEmail=document.getElementById("user-email");
-const password=document.getElementById("password");
+const passWord=document.getElementById("password");
 const signup=document.getElementById("signup");
 
 signup.addEventListener('click',(e)=>{
-    if (!userName.value||!userEmail.value||!password.value){
+    if (!userName.value||!userEmail.value||!passWord.value){
         alert("All fields are mandatory!")
     }
-    console.log(userName.value)
-    console.log(userEmail.value)
-    console.log(password.value)
+    e.preventDefault();
+    const  name=userName.value;
+    const  email=userEmail.value;
+    const  password=passWord.value;
+    axios({
+            method:'post',
+            url:`http://localhost:3000/users/add-user`,
+            data:{
+                name: name,
+                email: email,
+                password: password
+                }
+            }
+        )
+        .then(res=>{
+            console.log(res);
+        })
+        .catch((err)=>console.log(err));
 });
